1b322de67018feb97dfa35c3153d1ed9673e8212
Pradumon <88714734+Pradumon12@users.noreply.github.com>
2022-12-14 17:47:00 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,19 @@
-from dictionary import Dictionary
 
-d = Dictionary()
-word = input("Enter a word: ")
-meaning = d.meaning(word)
-print(meaning)
+# Import the necessary libraries
+import requests
+from bs4 import BeautifulSoup
+
+# Set the URL of the word to be searched
+url = "https://www.dictionary.com/browse/" + word
+
+# Make a request to the URL and store the response
+response = requests.get(url)
+
+# Parse the response using BeautifulSoup
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Extract the definition of the word from the soup
+definition = soup.find('div', class_='css-1o58fj8 e1hk9ate4').text
+
+# Print the definition of the word
+print(definition)
